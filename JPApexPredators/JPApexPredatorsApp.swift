02518fddd6104abb4b73f81161f72bb260b9fff8
DangNguyen <nguyendangit04@gmail.com>
2026-04-05 16:02:19 +0700

    Initial Commit

---
+++ JPApexPredators/JPApexPredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredatorsApp.swift
+//  JPApexPredators
+//
+//  Created by Dang Nguyen on 5/4/26.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
