fabd8efaa0d8b5f9c87d7af19d2111122553e41a
DangNguyen <nguyendangit04@gmail.com>
2026-04-18 17:33:04 +0700

    feat(list): load data to list

--- JPApexPredators/ApexPredator.swift
+++ JPApexPredators/ApexPredator.swift
@@ -5,19 +5,43 @@
 //  Created by Dang Nguyen on 5/4/26.
 //
 
-struct ApexPredator: Decodable {
+import Foundation
+import SwiftUI
+
+struct ApexPredator: Decodable, Identifiable {
     let id: Int
     let name: String
-    let type: String
+    let type: APType
     let latitude: Double
-    let longtitude: Double
+    let longitude: Double
     let movies: [String]
     let movieScenes: [MoviveScene]
     let link: String
+    
+    var image: String {
+        name.lowercased().replacingOccurrences(of: " ", with: "")
+    }
 }
 
 struct MoviveScene: Decodable {
     let id: Int
     let movie: String
     let sceneDescription: String
+}
+
+enum APType: String, Decodable {
+    case land
+    case air
+    case sea
+    
+    var background: Color {
+        switch self {
+        case .land:
+                .brown
+        case .air:
+                .teal
+        case .sea:
+                .blue
+        }
+    }
 }
